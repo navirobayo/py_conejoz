325777719b0c3c6407991a20c0503f9c38771911
Ivan <navirobayo@gmail.com>
2023-07-30 22:11:25 -0500

    App requirements

--- hfconejoz/__init__.py
+++ hfconejoz/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app():
     app = Flask (__name__)
 
-    from .main import main as main_blueprint
+    from .app import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
